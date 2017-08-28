d2eb5a32105e71a049d6072516cf1e725308068e
Joe Heitkamp <noetic97@gmail.com>
2017-08-27 21:31:42 -0600

    initial class setup

--- elevator.js
+++ elevator.js
@@ -1,5 +1,15 @@
 export default class Elevator {
   constructor() {
-    
+    this.floor = 0;
+    this.requests = [];
+    this.passengers = [];
+  }
+}
+
+export default class Person {
+  constructor(name) {
+    this.name = name;
+    this.currentFloor = 0;
+    this.dropOffFloor = 0;
   }
 }
